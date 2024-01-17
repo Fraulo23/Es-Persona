52d139558fa6007b90cb9cf335e81bdf33f296ce
Fraulo23 <fraulinifederico@gmail.com>
2024-01-17 16:35:29 +0100

    Aggiungere i file di progetto.

---
+++ Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Persona
+{
+    internal class Persona
+    {
+        public int AnnoDiNascita { get; set; }
+        public Persona(int annodinascita)
+        {
+            AnnoDiNascita = annodinascita;
+        }
+        public int CalcolaEta()
+        {
+            int Anno=DateTime.Now.Year;
+            return (Anno - AnnoDiNascita);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona(2004);
+            Console.WriteLine(p.CalcolaEta());
+            Console.ReadLine();
+        }
+    }
+}
